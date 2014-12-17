0e2d763134a5260624b24df249f6cb0d4ecfe808
Padmanaban <padmanaban.c@gmail.com>
2014-12-17 08:46:29 +0530

    new update

---
+++ Logout.aspx.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Security.Principal
+
+Partial Class Logout
+    Inherits System.Web.UI.Page
+
+
+
+    Protected Sub Logout_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        HttpContext.Current.User = New GenericPrincipal(New GenericIdentity(String.Empty), Nothing)
+        HttpContext.Current.Session.Clear()
+        HttpContext.Current.Session.Abandon()
+        HttpContext.Current.User = Nothing
+        Response.Redirect("Login.aspx")
+    End Sub
+End Class
